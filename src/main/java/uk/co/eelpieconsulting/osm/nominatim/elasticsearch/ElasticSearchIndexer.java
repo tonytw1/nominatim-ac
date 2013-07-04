0017781c991069daba0329ce04709ab6e4b76b12
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-07-04 11:04:17 +0100

    Import tuning.

--- src/main/java/uk/co/eelpieconsulting/osm/nominatim/elasticsearch/ElasticSearchIndexer.java
+++ src/main/java/uk/co/eelpieconsulting/osm/nominatim/elasticsearch/ElasticSearchIndexer.java
@@ -1,7 +1,10 @@
 package uk.co.eelpieconsulting.osm.nominatim.elasticsearch;
 
+import org.apache.log4j.Logger;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.common.joda.time.DateTime;
+import org.joda.time.Duration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +15,10 @@ import uk.co.eelpieconsulting.osm.nominatim.parsing.PlacesDumpParser;
 
 @Component
 public class ElasticSearchIndexer implements LineIndexer {
+	
+	private static final int COMMIT_SIZE = 10000;
+
+	private static Logger log = Logger.getLogger(ElasticSearchIndexer.class);
 
 	private final ElasticSearchClientFactory elasticSearchClientFactory;
 	private final JsonSerializer jsonSerializer;
@@ -25,18 +32,31 @@ public class ElasticSearchIndexer implements LineIndexer {
 	@Override
 	public void indexLines(PlacesDumpParser parser) {
 		final Client client = elasticSearchClientFactory.getClient();
-		final BulkRequestBuilder bulkRequest = client.prepareBulk();
+		BulkRequestBuilder bulkRequest = client.prepareBulk();
 		
+		int count = 0;
+		DateTime countStart = DateTime.now();
 		while (parser.hasNext()) {
 			Place place = parser.next();
-			System.out.println(place);
+			count++;
 			
 			final String placeJson = jsonSerializer.serialize(place);
-			System.out.println(placeJson);
-			bulkRequest.add(client.prepareIndex("osm", "places", place.getOsmId() + place.getType()).setSource(placeJson));             
-              
-			bulkRequest.execute().actionGet(); 
-		}		
+			bulkRequest.add(client.prepareIndex("osm", "places", place.getOsmId() + place.getType()).setSource(placeJson));
+			
+			if (count == COMMIT_SIZE) {
+				bulkRequest.execute().actionGet();
+				
+				Duration duration = new Duration(countStart.getMillis(), DateTime.now().getMillis());
+				log.info("Imported " + COMMIT_SIZE + " in " + duration + " at a rate of " + COMMIT_SIZE / duration.getMillis() + " per milli second");
+				count = 0;
+				countStart = DateTime.now();
+				bulkRequest = client.prepareBulk();
+			}			
+		}
+		
+		if (count > 0) {
+			bulkRequest.execute().actionGet();
+		}
 	}
 
 }
