5874a31d9550c02d43840eb71c7c8211374b59fb
Tony McCrae <tony@eelpieconsulting.co.uk>
2014-08-19 16:55:15 +0100

    Extract country code and lat/long to allow filtering of results from multiple country files

--- dump.pl
+++ dump.pl
@@ -2,7 +2,7 @@
 
 use DBI;
 
-my $dbh = DBI->connect("DBI:Pg:dbname=nominatim", "osm", "", {'RaiseError' => 1});
+my $dbh = DBI->connect("DBI:Pg:dbname=nominatim;port=5433", "tony", "", {'RaiseError' => 1});
 
 my $limits_sth = $dbh->prepare("select min(place_id) as start, max(place_id) as end from placex");
 $limits_sth->execute();
@@ -13,12 +13,14 @@ my $end = $limits_ref->{'end'};
 $limits_sth->finish();
 
 my $sth = $dbh->prepare("select osm_id, osm_type, class, type, housenumber, 
-	get_address_by_language(place_id,  ARRAY['name']) AS label from placex WHERE place_id >= ? AND place_id < ?");
+	get_address_by_language(place_id,  ARRAY['name']) AS label, calculated_country_code AS country, ST_Y(centroid) as latitude, ST_X(centroid) as longitude from placex WHERE place_id >= ? AND place_id < ?");
 for ($i = $start; $i < $end; $i = $i + 1000) {
 	warn "$i/$end\n";
 	$sth->execute($i, $i + 1000);      
 	while(my $ref = $sth->fetchrow_hashref()) {
-	    print "$ref->{'osm_id'}|$ref->{'osm_type'}|$ref->{'housenumber'}|$ref->{'label'}|$ref->{'class'}|$ref->{'type'}\n";
+	    my $line = "$ref->{'osm_id'}|$ref->{'osm_type'}|$ref->{'housenumber'}|$ref->{'label'}|$ref->{'class'}|$ref->{'type'}|$ref->{'country'}|$ref->{'latitude'}|$ref->{'longitude'}";
+            $line =~ s/\s/ /g;
+            print "$line\n";
 	}
 }
 
