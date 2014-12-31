c214fc0365aa9e895f2fdd1c49121abeed8b6202
Tony McCrae <tony@heathfield.eelpieconsulting.co.uk>
2014-12-31 23:57:45 +0000

    rank_address could be useful in extracting the high level features quickly

--- dump-ordered.pl
+++ dump-ordered.pl
@@ -19,9 +19,11 @@ my $sth = $dbh->prepare("select osm_id, osm_type, class, type, housenumber,
         get_address_by_language(place_id, ARRAY['name:en']) AS en_label, 
         calculated_country_code AS country,
         case when GeometryType(geometry) = 'POINT' then ST_Y(geometry) else ST_Y(centroid) end as latitude,
-        case when GeometryType(geometry) = 'POINT' then ST_X(geometry) else ST_X(centroid) end as longitude
+        case when GeometryType(geometry) = 'POINT' then ST_X(geometry) else ST_X(centroid) end as longitude,
+	rank_address
         FROM placex
         WHERE osm_id >= ? AND osm_id < ? AND osm_type='N'
+	AND rank_address > 0 and rank_address <= 16
 	ORDER by osm_id, osm_type
 	"
 );
@@ -30,7 +32,7 @@ my $chunksize = 1000;
 for (my $i = 0; $i < $end; $i = $i + $chunksize) {
 	$sth->execute($i, $i + $chunksize);
 	while(my $ref = $sth->fetchrow_hashref()) {
-	    my $line = "$ref->{'osm_id'}|$ref->{'osm_type'}|$ref->{'housenumber'}|$ref->{'label'}|$ref->{'en_label'}|$ref->{'class'}|$ref->{'type'}|$ref->{'country'}|$ref->{'latitude'}|$ref->{'longitude'}";
+	    my $line = "$ref->{'osm_id'}|$ref->{'osm_type'}|$ref->{'housenumber'}|$ref->{'label'}|$ref->{'en_label'}|$ref->{'class'}|$ref->{'type'}|$ref->{'country'}|$ref->{'latitude'}|$ref->{'longitude'}|$ref->{'rank_address'}";
             $line =~ s/\s/ /g;
             print "$line\n";
 	}
