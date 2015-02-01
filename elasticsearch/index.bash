dd07301ee032e0baf135f6fd65a1cdf1d115019b
tony <tony@eelpieconsulting.co.uk>
2015-02-01 17:23:15 +0000

    Lowercase field mapping

--- elasticsearch/index.bash
+++ elasticsearch/index.bash
@@ -1,2 +1,19 @@
-curl -XPUT http://localhost:9200/osm/
+curl -XDELETE 'http://localhost:9200/osm/'
+
+curl -XPUT http://localhost:9200/osm/ -d '
+{
+	"settings":{
+		"index":{
+			"analysis":{
+				"analyzer":{
+					"analyzer_startswith":{
+						"tokenizer":"keyword",
+						"filter":"lowercase"
+					}
+				}
+			}
+		}
+	}
+}
+'
 
--- elasticsearch/mappings.bash
+++ elasticsearch/mappings.bash
@@ -4,7 +4,11 @@ curl -XPUT 'http://localhost:9200/osm/places/_mapping' -d '
 {
 	"places" : { 
 		"properties" : { 
-			"address" : {"type" : "string", "index" : "not_analyzed" },
+			"address" : {
+				"type":"string",
+				"search_analyzer":"analyzer_startswith",
+				"index_analyzer":"analyzer_startswith"		
+			},
 			"classification" : {"type" : "string", "index" : "not_analyzed" },	
 			"type" : {"type" : "string", "index" : "not_analyzed" },	
 			"rank" : {"type" : "integer", "index" : "not_analyzed" },
@@ -12,6 +16,6 @@ curl -XPUT 'http://localhost:9200/osm/places/_mapping' -d '
 			"latlong" : {"type" : "geo_point"},
 			"country" : {"type" : "string", "index" : "not_analyzed" }
 		} 
-	} 
+	}
 }
 '
