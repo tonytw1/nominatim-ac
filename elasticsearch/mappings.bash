a3108d8f3b74530e9e9fb1c75cc54e1ef5436d2b
tony <tony@eelpieconsulting.co.uk>
2015-02-01 16:53:58 +0000

    Formatting

--- elasticsearch/mappings.bash
+++ elasticsearch/mappings.bash
@@ -1,12 +1,17 @@
 curl -XDELETE 'http://localhost:9200/osm/places/_mapping'
+
 curl -XPUT 'http://localhost:9200/osm/places/_mapping' -d '
-{ "places" : { "properties" : { 
-	"address" : {"type" : "string", "index" : "not_analyzed" },
-	"classification" : {"type" : "string", "index" : "not_analyzed" },	
-	"type" : {"type" : "string", "index" : "not_analyzed" },	
-	"rank" : {"type" : "integer", "index" : "not_analyzed" },
-	"tags" : {"type" : "string", "index" : "not_analyzed" },	
-	"latlong" : {"type" : "geo_point"},
-	"country" : {"type" : "string", "index" : "not_analyzed" }
-	} } }
+{
+	"places" : { 
+		"properties" : { 
+			"address" : {"type" : "string", "index" : "not_analyzed" },
+			"classification" : {"type" : "string", "index" : "not_analyzed" },	
+			"type" : {"type" : "string", "index" : "not_analyzed" },	
+			"rank" : {"type" : "integer", "index" : "not_analyzed" },
+			"tags" : {"type" : "string", "index" : "not_analyzed" },	
+			"latlong" : {"type" : "geo_point"},
+			"country" : {"type" : "string", "index" : "not_analyzed" }
+		} 
+	} 
+}
 '
