8d240316715fe4b340691871e5158b2a87e5ea28
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-02-03 18:49:51 +0000

    Enables static files.

--- src/main/java/uk/co/eelpieconsulting/osm/nominatim/Main.java
+++ src/main/java/uk/co/eelpieconsulting/osm/nominatim/Main.java
@@ -2,19 +2,19 @@ package uk.co.eelpieconsulting.osm.nominatim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackages="uk.co.eelpieconsulting")
 @Configuration
 @EnableScheduling
 @EnableWebMvc
-public class Main extends WebMvcConfigurerAdapter {
+public class Main extends WebMvcAutoConfiguration {
 	
 	private static ApplicationContext ctx;
     
