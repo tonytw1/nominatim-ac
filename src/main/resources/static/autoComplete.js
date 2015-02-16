9dd60045e59f98e8d97554ab5d5c377756dd6949
tony <tony@eelpieconsulting.co.uk>
2015-02-16 16:27:46 +0000

    Trying to disable jquery cache busting.

--- src/main/resources/static/autoComplete.js
+++ src/main/resources/static/autoComplete.js
@@ -2,21 +2,20 @@ $( "#location" ).autocomplete({
 	source: function( request, response ) {
 		$.ajax({
 			url: "/suggest",
-			dataType: "jsonp",
+			cache: true,
+			dataType: "json",
 			data: {
 				q: request.term							
 			},
 			success: function( data ) {
-				response( $.map( data, function( item ) {
-				
-					console.log(item);
+				response( $.map( data, function( item ) {				
 					return {
 						label: (item.address + " (" + item.classification + "/" + item.type + ")"),
 						value: item.address,
 						osmId: item.osmId,
 						osmType: item.osmType,
 						latitude: item.latlong.lat,
-						longitude: item.latlong.lon							
+						longitude: item.latlong.lon					
 					}
 				}));
 			}
