98e6490883d92e9b0d13d12001d9495a08bb0b82
Tony McCrae <tony@eelpieconsulting.co.uk>
2015-08-10 21:21:22 +0100

    WIP - Elastic may actually be a good place to store the elastic indexing watermark.

--- src/main/java/uk/co/eelpieconsulting/osm/nominatim/indexing/PartialIndexWatermarkService.java
+++ src/main/java/uk/co/eelpieconsulting/osm/nominatim/indexing/PartialIndexWatermarkService.java
@@ -1,23 +1,59 @@
 package uk.co.eelpieconsulting.osm.nominatim.indexing;
 
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+
+import java.util.Map;
+
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
 import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import uk.co.eelpieconsulting.common.views.json.JsonSerializer;
+import uk.co.eelpieconsulting.osm.nominatim.elasticsearch.ElasticSearchClientFactory;
+import uk.co.eelpieconsulting.osm.nominatim.elasticsearch.ElasticSearchIndexer;
+
+import com.google.common.collect.Maps;
 
 @Component
 public class PartialIndexWatermarkService {
-	
-	private DateTime watermark;
-	
-	public PartialIndexWatermarkService() {
-		this.watermark = new DateTime(2015, 3, 29, 0, 0, 0);	// TODO persist
+
+	private static final String WATERMARK = "watermark";
+
+	private ElasticSearchClientFactory elasticSearchClientFactory;
+
+	@Autowired
+	public PartialIndexWatermarkService(ElasticSearchClientFactory elasticSearchClientFactory) {
+		this.elasticSearchClientFactory = elasticSearchClientFactory;
 	}
 
 	public DateTime getWatermark() {
-		return watermark;
+		SearchRequestBuilder request = elasticSearchClientFactory.getClient().prepareSearch(ElasticSearchIndexer.INDEX).setTypes(WATERMARK).setQuery(boolQuery()).setSize(1);
+		SearchResponse searchResponse = request.get();
+		if (searchResponse.getHits().getTotalHits() == 0) {
+			return null;
+		}
+
+		return null;	// TODO
 	}
-	
+
 	public void setWatermark(DateTime watermark) {
-		this.watermark = watermark;		
+		final Client client = elasticSearchClientFactory.getClient();
+
+		Map<String, Object> map = Maps.newHashMap();
+		map.put("watermark", Long.toString(watermark.getMillis()));
+		String json = new JsonSerializer().serialize(map);
+
+		try {
+			IndexRequest indexRequest = new IndexRequest().index(ElasticSearchIndexer.INDEX).type(WATERMARK).id("1").source(json);
+			client.index(indexRequest).get();
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
-	
+
 }
