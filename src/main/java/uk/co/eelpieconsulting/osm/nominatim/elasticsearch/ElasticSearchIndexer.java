9daf357931f1be9ec8f5ba0f81e0a236ce0920f7
Tony McCrae <tony@eelpieconsulting.co.uk>
2017-01-03 21:49:52 +0000

    Refactor; split reading of places from indexing.

--- src/main/java/uk/co/eelpieconsulting/osm/nominatim/elasticsearch/ElasticSearchIndexer.java
+++ src/main/java/uk/co/eelpieconsulting/osm/nominatim/elasticsearch/ElasticSearchIndexer.java
@@ -1,5 +1,6 @@
 package uk.co.eelpieconsulting.osm.nominatim.elasticsearch;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import org.apache.log4j.Logger;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
@@ -24,49 +25,45 @@ public class ElasticSearchIndexer {
 	private static final Set<String> IGNORED_TAG_CLASSIFICATIONS = Sets.newHashSet("wikipedia", "description", "attribution", "population", "name:prefix", "website");	// TODO is this optimisation actualy useful?
 
 	public static final String TYPE = "places";
-	private static final int COMMIT_SIZE = 10000;
+	private static final int COMMIT_SIZE = 1000;
 	
 	private final ElasticSearchClientFactory elasticSearchClientFactory;
 	private final JsonSerializer jsonSerializer;
 
 	private final String writeIndex;
-	
+
+	private final Client client;
+
 	@Autowired
-	public ElasticSearchIndexer(ElasticSearchClientFactory elasticSearchClientFactory,
-			@Value("${elasticsearch.index.write}") String writeIndex) {
+	public ElasticSearchIndexer(ElasticSearchClientFactory elasticSearchClientFactory, @Value("${elasticsearch.index.write}") String writeIndex) {
 		this.elasticSearchClientFactory = elasticSearchClientFactory;
 		this.writeIndex = writeIndex;
 		this.jsonSerializer = new JsonSerializer();
+		client = elasticSearchClientFactory.getClient();
 	}
 	
 	public void indexLines(OsmPlacesSource osmPlacesSource) {
-		final Client client = elasticSearchClientFactory.getClient();
-				
 		log.info("Importing records");
 		BulkRequestBuilder bulkRequest = client.prepareBulk();		
 
-		int count = 0;
+		List<Place> places = Lists.newArrayList();
 		DateTime countStart = DateTime.now();
 		while (osmPlacesSource.hasNext()) {
 			Place place = osmPlacesSource.next();
-			count++;
+			places.add(place);
 
-			final String placeJson = jsonSerializer.serialize(place);
-			bulkRequest.add(client.prepareIndex(writeIndex, TYPE, place.getOsmId() + place.getOsmType()).setSource(placeJson));
-			
-			if (count == COMMIT_SIZE) {
-				bulkRequest.execute().actionGet();
+			if (places.size() == COMMIT_SIZE) {
+				index(places);
 				
 				Duration duration = new Duration(countStart.getMillis(), DateTime.now().getMillis());
 				log.info("Imported " + COMMIT_SIZE + " in " + duration.getMillis());
-				count = 0;
+				places = Lists.newArrayList();
 				countStart = DateTime.now();
-				bulkRequest = client.prepareBulk();
-			}			
+			}
 		}
-		
-		if (count > 0) {
-			bulkRequest.execute().actionGet();
+
+		if (!places.isEmpty()) {
+			index(places);
 		}
 		
 		log.info("Import completed");
@@ -75,7 +72,6 @@ public class ElasticSearchIndexer {
 	public void index(List<Place> places) {
 		log.info("Importing updates");
 
-		final Client client = elasticSearchClientFactory.getClient();		
 		BulkRequestBuilder bulkRequest = client.prepareBulk();
 		for (Place place : places) {
 			bulkRequest.add(client.prepareIndex(writeIndex, TYPE, place.getOsmId() + place.getOsmType()).setSource(jsonSerializer.serialize(place)));
