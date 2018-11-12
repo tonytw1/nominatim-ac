a8a78058b80e1d6e92dde8f8bdeae3cab688605a
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-11-12 21:06:29 +0000

    Use default credentials for a Nominatim 3.1 Docker image; provides a failing test for a likely change in the place name function.
    OsmPlacesSourceTest > canExtractDisplayAddressForPlace STANDARD_ERROR
        org.postgresql.util.PSQLException: ERROR: function get_address_by_language(bigint, text[]) does not exist
          Hint: No function matches the given name and argument types. You might need to add explicit type casts.

--- src/test/java/OsmPlacesSourceIT.java
+++ src/test/java/OsmPlacesSourceIT.java
@@ -12,60 +12,60 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class OsmPlacesSourceIT {
-	
-	private static final String DATABASE_HOST = "localhost";
-	private static final String DATABASE_USER = "www-data";
-	private static final String DATABASE_PASSWORD = "www";
 
-	private OsmDAO osmDAO;
-	private PlaceRowParser placeRowParser;
+  private static final String DATABASE_HOST = "localhost";
+  private static final String DATABASE_USER = "www-data";
+  private static final String DATABASE_PASSWORD = "www";
 
-	@Before
-	public void setup() throws SQLException {
-		placeRowParser = new PlaceRowParser();
-		osmDAO = new OsmDAO(DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST);
-	}
-	
-	//@Test
-	public void canIterateThroughPlaces() throws Exception {
-		OsmPlacesSource osmPlacesSource = new OsmPlacesSource(osmDAO, placeRowParser, "R");
-		int c = 0;
-		while(osmPlacesSource.hasNext() && c < 10000) {
-			System.out.println(osmPlacesSource.next());
-			c++;
-		}
-	}
-	
-	@Test
-	public void canExtractDisplayAddressForPlace() throws Exception {
-		final ResultSet placeRow = osmDAO.getPlace(284926920, "W");
-		placeRow.next();
+  private OsmDAO osmDAO;
+  private PlaceRowParser placeRowParser;
 
-		final Place place = placeRowParser.buildPlaceFromCurrentRow(placeRow);
-		
-		assertEquals("Twickenham Rowing Club, Church Lane, Cole Park, Twickenham, London Borough of Richmond upon Thames, London, Greater London, England, TW1 3DU, United Kingdom", place.getAddress());
-	}
-	
-	@Test
-	public void nameShouldUseNameInPreferenceToRefField() throws Exception {
-		final ResultSet placeRow = osmDAO.getPlace(202880711, "W");
-		placeRow.next();
-		
-		final Place place = placeRowParser.buildPlaceFromCurrentRow(placeRow);
+  @Before
+  public void setup() throws SQLException {
+    placeRowParser = new PlaceRowParser();
+    osmDAO = new OsmDAO(DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST);
+  }
 
-		assertEquals("Arras Tunnel, Te Aro, Wellington, Wellington City, Wellington, 6011, New Zealand", place.getAddress());
-	}
+  //@Test
+  public void canIterateThroughPlaces() throws Exception {
+    OsmPlacesSource osmPlacesSource = new OsmPlacesSource(osmDAO, placeRowParser, "R");
+    int c = 0;
+    while (osmPlacesSource.hasNext() && c < 10000) {
+      System.out.println(osmPlacesSource.next());
+      c++;
+    }
+  }
 
-	@Test
-	public void placeTagsShouldIncludeTheClassificationTypeAndExtraTags() throws Exception {
-		final ResultSet placeRow = osmDAO.getPlace(1643367, "R");
-		placeRow.next();
+  @Test
+  public void canExtractDisplayAddressForPlace() throws Exception {
+    final ResultSet placeRow = osmDAO.getPlace(284926920, "W");
+    placeRow.next();
 
-		final Place place = placeRowParser.buildPlaceFromCurrentRow(placeRow);
+    final Place place = placeRowParser.buildPlaceFromCurrentRow(placeRow);
 
-		assertTrue(place.getTags().contains("boundary|national_park"));
-		assertTrue(place.getTags().contains("wikipedia|en:Yosemite National Park"));
-		assertTrue(place.getTags().contains("website|http://www.nps.gov/yose/"));
-	}
+    assertEquals("Twickenham Rowing Club, Church Lane, Cole Park, Twickenham, London Borough of Richmond upon Thames, London, Greater London, England, TW1 3DU, United Kingdom", place.getAddress());
+  }
+
+  @Test
+  public void nameShouldUseNameInPreferenceToRefField() throws Exception {
+    final ResultSet placeRow = osmDAO.getPlace(202880711, "W");
+    placeRow.next();
+
+    final Place place = placeRowParser.buildPlaceFromCurrentRow(placeRow);
+
+    assertEquals("Arras Tunnel, Te Aro, Wellington, Wellington City, Wellington, 6011, New Zealand", place.getAddress());
+  }
+
+  @Test
+  public void placeTagsShouldIncludeTheClassificationTypeAndExtraTags() throws Exception {
+    final ResultSet placeRow = osmDAO.getPlace(1643367, "R");
+    placeRow.next();
+
+    final Place place = placeRowParser.buildPlaceFromCurrentRow(placeRow);
+
+    assertTrue(place.getTags().contains("boundary|national_park"));
+    assertTrue(place.getTags().contains("wikipedia|en:Yosemite National Park"));
+    assertTrue(place.getTags().contains("website|http://www.nps.gov/yose/"));
+  }
 
 }
