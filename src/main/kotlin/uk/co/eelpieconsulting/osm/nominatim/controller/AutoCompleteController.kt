0417fed1e00e3e2952b8b75cf7bc2d43d9dd70ed
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-11-14 20:53:02 +0000

    Imports.

--- src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/controller/AutoCompleteController.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/controller/AutoCompleteController.kt
@@ -2,7 +2,6 @@ package uk.co.eelpieconsulting.osm.nominatim.controller
 
 import org.joda.time.format.ISODateTimeFormat
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.co.eelpieconsulting.osm.nominatim.elasticsearch.ElasticSearchAutoCompleteService
 import uk.co.eelpieconsulting.osm.nominatim.indexing.PartialIndexWatermarkService
