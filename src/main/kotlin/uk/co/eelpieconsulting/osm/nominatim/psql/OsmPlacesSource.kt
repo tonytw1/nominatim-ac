1350f9a2ee7b913c2c97433175208e0e65cf4519
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-11-14 11:41:10 +0000

    Move to Kotlin so that select can be past in a lambda.

---
+++ src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/psql/OsmPlacesSource.kt
@@ -0,0 +1,64 @@
+package uk.co.eelpieconsulting.osm.nominatim.psql
+
+import org.apache.log4j.Logger
+import uk.co.eelpieconsulting.osm.nominatim.model.Place
+
+import java.sql.ResultSet
+import java.sql.SQLException
+
+class OsmPlacesSource @Throws(SQLException::class)
+constructor(private val osmDAO: OsmDAO, private val placeRowParser: PlaceRowParser, private val type: String) {
+
+    private var places: ResultSet? = null
+    private var start: Long = 0
+
+    private val max: Long
+
+    private val log = Logger.getLogger(OsmPlacesSource::class.java)
+
+    private val STEP_SIZE: Long = 1000
+    
+    init {
+        start = 0
+        this.max = osmDAO.getMax(type)
+        this.places = prepare(start, STEP_SIZE)
+    }
+
+    private fun prepare(start: Long, stepSize: Long): ResultSet {
+        log.info("Preparing type: $type $start")
+        try {
+            return osmDAO.getPlaces(start, stepSize, type)
+        } catch (e: SQLException) {
+            throw RuntimeException(e)
+        }
+
+    }
+
+    operator fun hasNext(): Boolean {
+        return start < max
+    }
+
+    operator fun next(): Place {
+        try {
+            if (places!!.isLast) {
+                log.debug("After last; preparing again")
+                this.places = prepare(start, STEP_SIZE)
+            }
+            places!!.next()
+
+        } catch (e: SQLException) {
+            throw RuntimeException(e)
+        }
+
+        try {
+            val place = placeRowParser.buildPlaceFromCurrentRow(places!!)
+            start = place.osmId
+            return place
+
+        } catch (e: SQLException) {
+            throw RuntimeException(e)
+        }
+
+    }
+
+}
