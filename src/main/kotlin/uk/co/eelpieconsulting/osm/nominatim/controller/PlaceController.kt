9eff3f71ed1d2e38c8e0bc68d3d3a42e527a7590
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-11-14 14:32:01 +0000

    Reuse place extractor.

--- src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/controller/PlaceController.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/controller/PlaceController.kt
@@ -1,20 +1,20 @@
 package uk.co.eelpieconsulting.osm.nominatim.controller
 
-import com.google.common.collect.Lists
-import com.google.common.collect.Sets
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
 import uk.co.eelpieconsulting.osm.nominatim.model.Place
 import uk.co.eelpieconsulting.osm.nominatim.psql.OSMDAOFactory
+import uk.co.eelpieconsulting.osm.nominatim.psql.OsmPlacesSource
+import uk.co.eelpieconsulting.osm.nominatim.psql.PlaceExtractor
 import uk.co.eelpieconsulting.osm.nominatim.psql.PlaceRowParser
 import uk.co.eelpieconsulting.osm.nominatim.views.ViewFactory
 import java.sql.SQLException
 import java.util.regex.Pattern
 
 @Controller
-class PlaceController(private val viewFactory: ViewFactory, osmDAOFactory: OSMDAOFactory, private val placeRowParser: PlaceRowParser) {
+class PlaceController(private val viewFactory: ViewFactory, osmDAOFactory: OSMDAOFactory, private val placeRowParser: PlaceRowParser, placeExtractor: PlaceExtractor) {
 
     private val OSM_IDENTIFIER_FORMAT = Pattern.compile("^(\\d+)(R|W|N)$")
     private val osmDAO = osmDAOFactory.build()
@@ -27,17 +27,18 @@ class PlaceController(private val viewFactory: ViewFactory, osmDAOFactory: OSMDA
             val osmId = java.lang.Long.parseLong(matcher.group(1))
             val osmType = matcher.group(2)
 
-            val placeRows = osmDAO.getPlace(osmId, osmType)
-            var currentPlace: Place? = null
-            val currentTags = Sets.newHashSet<String>()
-            while (placeRows.next()) {
-                val place = placeRowParser.buildPlaceFromCurrentRow(placeRows)
-                currentPlace = place
-                currentTags.addAll(place.tags)    // TODO deduplicate with indexer
+            fun cursor(start: Long, pageSize: Long) = osmDAO.getPlace(osmId, osmType)
+
+            val source = OsmPlacesSource(osmDAO, placeRowParser, ::cursor)
+
+            var places = emptyList<Place>()
+            fun collectPages(place: Place) {
+                places = places + place
             }
 
-            currentPlace!!.tags = Lists.newArrayList(currentTags)
-            return ModelAndView(viewFactory.jsonView).addObject("data", currentPlace)
+            PlaceExtractor().extractPlaces(source, ::collectPages)
+
+            return ModelAndView(viewFactory.jsonView).addObject("data", places.first())
 
         } else {
             throw IllegalArgumentException()
