f60b4ad5a8b35b8c35d90c99d363b599d586d55d
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-05-23 18:16:38 +0100

    Additional exclusions.

--- src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/elasticsearch/profiles/Everything.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/elasticsearch/profiles/Everything.kt
@@ -3,18 +3,19 @@ package uk.co.eelpieconsulting.osm.nominatim.elasticsearch.profiles
 import org.elasticsearch.index.query.BoolQueryBuilder
 import org.elasticsearch.index.query.QueryBuilders
 
-class Everything: Profile {
+class Everything : Profile {
 
     private val TAGS = "tags"
 
     override fun getName(): String {
-       return "everything"
+        return "everything"
     }
 
     override fun getQuery(): BoolQueryBuilder {
         val isBoundaryHistoric = QueryBuilders.termQuery(TAGS, "boundary|historic");
+        val isRailwayStop = QueryBuilders.termQuery(TAGS, "railway|stop");
 
-        return QueryBuilders.boolQuery().mustNot(isBoundaryHistoric)
+        return QueryBuilders.boolQuery().mustNot(isBoundaryHistoric).mustNot(isRailwayStop)
     }
 
 }
