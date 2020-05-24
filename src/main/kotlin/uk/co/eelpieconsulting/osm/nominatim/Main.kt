5bc73cfe59ea1ce6e136425f1d991f8ee305b01b
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-05-24 12:06:38 +0100

    Linting. Port Main from Java to Kotlin. Drop as many annotations as possible.

---
+++ src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/Main.kt
@@ -0,0 +1,11 @@
+package uk.co.eelpieconsulting.osm.nominatim;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Main
+
+fun main(args: Array<String>)  {
+    runApplication<Main>(*args)
+}
