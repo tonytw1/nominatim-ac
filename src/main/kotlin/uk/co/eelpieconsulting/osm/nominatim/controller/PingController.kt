57c912d943e3cab4d3d03607a537e8af35566a86
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-05-03 19:26:26 +0100

    Ping controller.

---
+++ src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/controller/PingController.kt
@@ -0,0 +1,14 @@
+package uk.co.eelpieconsulting.osm.nominatim.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PingController() {
+
+    @GetMapping("/healthz")
+    fun ping(): String {
+        return "ok"
+    }
+
+}
