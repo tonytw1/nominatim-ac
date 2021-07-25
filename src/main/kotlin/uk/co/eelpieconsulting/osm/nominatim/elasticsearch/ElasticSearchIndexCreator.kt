f821333722528065189b94690bbbf267e696ca85
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-07-25 12:59:48 +0100

    Create write index on demand.

---
+++ src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/elasticsearch/ElasticSearchIndexCreator.kt
@@ -0,0 +1,66 @@
+package uk.co.eelpieconsulting.osm.nominatim.elasticsearch
+
+import org.apache.log4j.Logger
+import org.elasticsearch.client.RequestOptions
+import org.elasticsearch.common.xcontent.XContentType
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class ElasticSearchIndexCreator @Autowired constructor(private val elasticSearchClientFactory: ElasticSearchClientFactory) {
+
+    private val log = Logger.getLogger(ElasticSearchIndexCreator::class.java)
+
+    fun ensureIndexExists(index: String): Boolean {
+        if (indexExists(index)) {
+            log.info("Found existing index $index; not recreating: " + index)
+            return true
+        }
+
+        // Create index
+        val indexDefinitionJson = """
+            {
+                "settings": {
+                    "index": {                    
+                        "analysis": {
+                            "analyzer":{
+                                "startswith":{
+                                    "tokenizer":"keyword",
+                                    "filter":"lowercase"
+                                }
+                            }
+                        }
+                    }
+                },
+                "mappings": {
+                    "properties" : { 
+                        "address" : {
+                            "type":"text",
+                            "search_analyzer":"startswith",
+                            "analyzer":"startswith"
+                        },
+                        "addressRank" : {"type" : "integer" },
+                        "adminLevel" : {"type" : "integer"},
+                        "tags" : {"type" : "keyword" },
+                        "latlong" : {"type" : "geo_point"},
+                        "country" : {"type" : "keyword" }
+                    }
+                }
+            }
+        """.trimIndent()
+
+        val req = org.elasticsearch.client.indices.CreateIndexRequest(index)
+        req.source(indexDefinitionJson, XContentType.JSON)
+
+        val createIndexResponse = elasticSearchClientFactory.getClient().indices().create(req, RequestOptions.DEFAULT)
+        log.info("Created index ${createIndexResponse.index()}")
+        return true
+    }
+
+    fun indexExists(index: String): Boolean {
+        val req = org.elasticsearch.client.indices.GetIndexRequest(index)
+        return elasticSearchClientFactory.getClient().indices().exists(req, RequestOptions.DEFAULT)
+    }
+
+}
+
