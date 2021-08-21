b2b77d979f114ba3a8a991c81b9ea9e7ce69d647
Tony McCrae <tony@eelpieconsulting.co.uk>
2021-08-21 10:56:59 +0100

    Move to POST action.

--- src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/controller/ImportController.kt
+++ src/main/kotlin/uk/co/eelpieconsulting/osm/nominatim/controller/ImportController.kt
@@ -1,13 +1,13 @@
 package uk.co.eelpieconsulting.osm.nominatim.controller
 
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.co.eelpieconsulting.osm.nominatim.indexing.FullIndexBuilder
 
 @RestController
 class ImportController(val fullIndexBuilder: FullIndexBuilder) {
 
-    @GetMapping("/import")
+    @PostMapping("/import")
     fun inputIndex(): String {
         fullIndexBuilder.buildFullIndex()
         return "ok"
